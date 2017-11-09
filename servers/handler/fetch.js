8a98eab2269c7e410905f6379df0af4fa25f73fa
brchen <eric790620@gmail.com>
2017-11-09 14:17:14 +0800

    Update fetch empty page

--- servers/handler/fetch.js
+++ servers/handler/fetch.js
@@ -1,7 +1,41 @@
 (()=>{
 	"use strict";
+
+	const layout = require('layout');
+    const body = require('body');
+	const list = require('list');
+
+	function __GET_INFO() {
+
+		return new Promise((fulfill, reject)=>{
+			fulfill([]);
+		});
+	}
+
+	function __WRITE_RESPONSE(res, data) {
+		
+		res.writeHead(200, { 'Content-Type': 'text/html' });
+		res.write(layout({
+			js: [
+				'./res/js/index.js'
+			],
+			css: [
+				'./res/css/style.css'
+			],
+			body: body([
+				list(data)
+			])
+		}));
+		res.end();
+	}
 	
 	let fetch = module.exports = (control, result=null)=>{
 	
+		const {request:req, response:res} = control.env;
+
+		return __GET_INFO()
+		.then((value)=>{
+			__WRITE_RESPONSE(res, value);
+		});
 	};
 })();
