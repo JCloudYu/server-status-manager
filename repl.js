7f7b3f007abac9c04cf905d17ce483806df88b1e
JCloudYu <j.cloud.yu@gmail.com>
2017-11-09 17:02:52 +0800

    Completely rewrite repl runtime

--- repl.js
+++ repl.js
@@ -1,74 +1,82 @@
 (()=>{
 	"use strict";
 	
-	const net		 = require( 'net' );
+	require( 'colors' );
+	require( 'tiinytiny' ).register();
 	const repl		 = require( 'repl' );
 	const config	 = require( 'json-cfg' ).trunk;
-	const EvtEmitter = require( 'events' );
-	config.conf.http = config.conf.http || {};
+	const jwt		 = require( 'lib-jwt' );
+	const moment	 = require( 'moment' );
+	const crypto	 = require( 'crypto' );
 	
-	
-	
-	let __evtPool	 = new EvtEmitter();
-	let __replServer = net.createServer();
-	let __exposedAPI = Object.imprintProperties({}, {
-		genToken:(identity, duration=31536000)=>{
+	const argv = process.argv.slice( 2 );
+	while( argv.length > 0 ) {
+		switch( argv.shift() ) {
+			case "--conf":
+				let opt = argv.shift();
+				console.log( "Config assigned! Loading..." );
+				config.load(opt);
+				break;
 			
+			default:
+				break;
+		}
+	}
+	
+	
+	
+	
+	const ACCESS_TOKEN_GENERATOR = {
+		'access-ticket': (options)=>{
+			let duration = options.duration || 3600;
+		
+			return jwt.genToken(
+				{ typ:'JWT', alg:'HS256' },
+				{
+					type:'access-ticket',
+					exp: moment().unix() + duration
+				}
+			);
+		},
+		'update-ticket': (options)=>{
+			let duration = options.duration || 31536000;
+			let identity = options.identity || 'unknown';
+			if ( identity === 'unknown' ) {
+				throw `Missing necessary data \`identity\``;
+			}
+		
+			return jwt.genToken(
+				{ typ:'JWT', alg:'HS256' },
+				{
+					type: 'update-ticket',
+					exp: moment().unix() + duration,
+					identity: identity
+				}
+			);
+		}
+	};
+	const __exposedAPI = Object.imprintProperties({}, {
+		genSecret:()=>{
+			return jwt.Base64Url.encode(crypto.randomBytes(128));
+		},
+		genToken:(type='update-ticket', options={})=>{
+			let generator = ACCESS_TOKEN_GENERATOR[type];
+			if ( !generator ) {
+				throw `Unsupported access token type \`${type}\`!`;
+			}
+			
+			return generator(options);
 		}
 	});
 	
-	const __exports  = module.exports = {
-		init:()=>{
-			if (!config.conf.repl.enabled) {
-				return;
-			}
-			
-			return new Promise((fulfill, reject)=>{
-				__replServer.on('connection', (socket)=>{
-					const session = repl.start({
-						prompt: `${socket.remoteAddress}> `,
-						input: socket,
-						output: socket
-					});
-					
-					session.on('exit', () => {
-						socket.end();
-					});
-				});
-				
-				fulfill();
-			});
-		},
-		cleanup:()=>{
-			if ( !__replServer ) return;
-			
-			__replServer.close(); __replServer = null;
-			__evtPool.removeAllListeners(); __evtPool = null;
-		},
-		serve: ()=>{
-			if (!config.conf.repl.enabled) {
-				return;
-			}
-			
-			return new Promise((fulfill, reject)=>{
-				try {
-					let conf = config.conf.repl || {};
-					let port = conf.port || 8180;
-					let host = conf.port || 'localhost';
-					STDOUT( `Start accepting repl request on ${host.toString().yellow}:${port.toString().yellow} => ` );
-					__replServer.listen(port, host);
-					STDOUT( "Success".green, true );
-					fulfill();
-				}
-				catch(err) {
-					STDOUT( "Fail".red, true );
-					reject(err);
-				}
-			});
-		},
-		on:(...args)=>{
-			__evtPool.on(...args);
-			return __exports;
-		}
-	};
+	
+	
+	console.log( `Starting repl mode with following configurations:` );
+	console.log( `${'Secret'.green}: ${(config.conf.secret || '').yellow}` );
+	const session = repl.start({
+		prompt: 'state-helper> ',
+		replMode: repl.REPL_MODE_STRICT
+	});
+	require( 'repl.history')(session, `${__dirname}/.repl-history`);
+	session.context.admin = __exposedAPI;
 })();
