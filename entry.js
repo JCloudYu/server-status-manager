26fee45242af66e731433f049e8a258a654ea97e
JCloudYu <j.cloud.yu@gmail.com>
2017-11-16 22:04:34 +0800

    Completely isolate environmental initialization logics from boot script

--- entry.js
+++ entry.js
@@ -1,56 +1,21 @@
 (()=>{
 	"use strict";
-
-	global.__baseDir = __dirname;
 	
-	// add path
+	// Register default local module search paths
 	const appModulePath = require( 'app-module-path' );
 	appModulePath.addPath( `${__dirname}` );
-	appModulePath.addPath( `${__dirname}/fetch-mode` );
-	appModulePath.addPath( `${__dirname}/fetch-mode/modules` );
-	appModulePath.addPath( `${__dirname}/fetch-mode/tmpl` );
-
-	// config load must be on the top
-	require( 'colors' );
-	require( 'tiinytiny' ).register();
-	const config = require( 'json-cfg' ).trunk;	
-	const argv = process.argv.slice( 2 );
-	while( argv.length > 0 ) {
-		switch( argv.shift() ) {
-			case "--conf":
-				let opt = argv.shift();
-				console.log( "Config assigned! Loading...".white );
-				config.load(opt);
-				break;
-			
-			default:
-				break;
-		}
-	}
-
-	// create runtime config
-	Object.imprintProperties(config.conf, {
-		'paths': {
-			'__baseDir': __dirname,
-			'static': `${__dirname}/fetch-mode/static-resources`
-		}
-	}, [false,true,true]);
-
-	const http	 = require( 'servers/http' );
-	const db	 = require( 'lib/mongo' );
 	
-	global.STDOUT = (content, newLine=false)=>{
-		process.stdout.write(`${content}${newLine ? "\n" : ''}`);
-	};
+	// Initialize execution environment
+	require( './env' );
 
-	db.init()
-	.then(()=>{
-		return http.init()
-	})
-	.then(()=>{
-		return http.serve();
-	})
+	
+	
+	// Initialize runtime environments
+	const http = require( 'servers/http' );
+	const db   = require( 'lib/mongo' );
+	
+	db.init().then(http.init).then(http.serve)
 	.catch((err)=>{
-		console.log(err);
+		console.inspect(err);
 	});
 })();
---
+++ env.js
@@ -0,0 +1,75 @@
+/**
+ * Project: server-status-manager
+ * File: env.js
+ * Author: JCloudYu
+ * Create Date: Nov. 16, 2017
+ */
+(() => {
+	"use strict";
+	
+	const util = require( 'util' );
+	const path = require( 'path' );
+	const projRoot = path.resolve(path.dirname(require.main.filename));
+	
+	
+	
+	// Register environmentally global APIs
+	[ 'STDOUT', 'STDERR' ].forEach((name)=>{
+		global[name] = __OUTPUT_STREAM.bind(null, process[name.toLowerCase()]);
+	});
+	
+	console.inspect = (...args)=>{
+		args.forEach((arg)=>{
+			STDOUT(util.inspect(arg, {colors:true, depth:20}))
+		});
+	};
+	
+	
+	
+	// config load must be on the top
+	require( 'colors' );
+	require( 'tiinytiny' ).register();
+	
+	
+	
+	// Eat execution arguments
+	const env = {};
+	const argv = process.argv.slice( 2 );
+	while( argv.length > 0 ) {
+		switch( argv.shift() ) {
+			case "--conf":
+				env.conf = argv.shift();
+				break;
+			
+			default:
+				break;
+		}
+	}
+
+
+	
+	// Load environmental configurations
+	const config = require( 'json-cfg' ).trunk;
+	if ( !env.conf ) {
+		STDOUT( `No config file is assigned! Using default configurations!`.red );
+		env.conf = `${projRoot}/config.default.json`;
+	}
+	
+	STDOUT( `${'Loading config file'.green} \`${env.conf.yellow}\` ${'...'.green}` );
+	config.load( env.conf );
+
+
+
+	// Register runtime information
+	env.project_root = projRoot;
+	config.conf.env = Object.imprintProperties(config.conf.env||{}, env, [true, true, false]);
+	
+	
+	
+	
+	
+	
+	function __OUTPUT_STREAM(stream, content, newLine=true) {
+		stream.write(`${content}${newLine ? "\n" : ''}`);
+	}
+})();
