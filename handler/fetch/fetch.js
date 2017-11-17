09445103d854ab9e8ca5b64d029f522a220da794
JCloudYu <j.cloud.yu@gmail.com>
2017-11-17 08:53:58 +0800

    Rewrite data querying logic

--- handler/fetch/fetch.js
+++ handler/fetch/fetch.js
@@ -1,50 +1,23 @@
 (()=>{
 	"use strict";
 
-	const http	 = require('http');
-	const fs	 = require('fs');
 	const mongo	 = require('lib/mongo');
-	const config = require('json-cfg').trunk.conf;
 
 	const layout = require('./tmpl/layout');
     const body	 = require('./tmpl/body');
 	const list	 = require('./tmpl/list');
-
-	const __basePath = config.env.project_root;
-
+	
+	
+	
 	function __GET_SERVERS_INFO() {
-
-		return new Promise((fulfill, reject)=>{
-
-			let findQuery = {};
-			mongo.inst
-			.collection('latest')
-			.find(findQuery)		
-			.sort({init: -1})
-			.skip(0)
-			.limit(0)
-			.toArray()
-			.then((value)=>{
-				fulfill(value);
-			})
-			.catch(reject);
-		});
-	}
-
-	function __PURGE_INFO(infos) {
-
-		let listData = [];
-		infos.forEach(info => {
-			listData.push({
-				'identity': info.identity,
-				'time': info.time,
-				'cpu': info.state.cpu,
-				'mem': info.state.mem,
-				'disk': info.state.disk
-			});
-		});
-
-		return Promise.resolve(listData);
+		return mongo.inst.collection( 'latest' ).aggregate([
+			{ $match:{}},
+			{ $project:{
+				_id:1, identity:1, time:1,
+				cpu:"$state.cpu", mem:"$state.mem", disk:"$state.disk"
+			}},
+			{$sort:{init:-1}}
+		]).toArray();
 	}
 
 	function __WRITE_RESPONSE(res, data) {
@@ -66,11 +39,9 @@
 	}
 	
 	module.exports = (control, chainData)=>{
-	
 		const {request:req, response:res} = control.env;
-
+		
 		return __GET_SERVERS_INFO()
-		.then(__PURGE_INFO)
 		.then((value)=>{
 			__WRITE_RESPONSE(res, value);
 		})
